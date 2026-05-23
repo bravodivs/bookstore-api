9a9d4c10e283e947bab965f351b427abd5814cc1
devanshu <vermadevanshu17@gmail.com>
2026-05-23 22:47:39 +0530

    fixed catalog startup

--- catalog-service/src/main/java/api/bookstore/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/api/bookstore/catalog_service/CatalogServiceApplication.java
@@ -2,10 +2,10 @@ package api.bookstore.catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
