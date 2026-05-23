97cb5486677275459595b13af8b6dacf2531f071
devanshu <vermadevanshu17@gmail.com>
2026-05-23 22:54:10 +0530

    fixed inventory startup

--- inventory-service/src/main/java/api/bookstore/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/api/bookstore/inventory_service/InventoryServiceApplication.java
@@ -2,8 +2,10 @@ package api.bookstore.inventory_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 /// tracks stock availability for books and keeps other services informed.
 	public static void main(String[] args) {
